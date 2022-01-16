c05ebb9733c4a9d76e286241bcccf81382242563
gcancho <giovannicb19@gmail.com>
2022-01-15 20:17:15 -0500

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // BrowserRouter debe contener adentro a toda la aplicacion para que pueda funcionar
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>Mi blog</h1>
+          {/* En vez de usar 'a' usamos NavLink para que no se recargue la pagina, y el 'href' lo remplazamos por 'to' */}
+          <nav>
+            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/acerca-de">Acerca de</NavLink>
+          </nav>
+        </header>
+        <main>
+          {/* Dentro de 'routes' deben ir los 'route' sino no funciona */}
+          <Routes>
+            {/* Cada route es una ruta, con 'path' le indicamos el nombre de la ruta  */}
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h2>Pagina de Inicio</h2>
+                  <p>Esta en la pagina principal de nuestro sitio</p>
+                </div>
+              }
+            />
+            {/* Esta ruta se va a mostrar cuando tenga en su url '/blog' ya que asi esta definido en el path */}
+            <Route
+              path="/blog"
+              element={
+                <div>
+                  <h2>Blog</h2>
+                  <ul>
+                    <li>Articulo #1</li>
+                    <li>Articulo #2</li>
+                    <li>Articulo #3</li>
+                  </ul>
+                </div>
+              }
+            />
+            <Route
+              path="/acerca-de"
+              element={
+                <div>
+                  <h2>Acerca de</h2>
+                  <p>Hola me llamo Giovanni</p>
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
